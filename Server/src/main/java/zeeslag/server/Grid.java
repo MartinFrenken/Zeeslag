a2fa9f532c9cfac5d80152d7c41c30d89080c7c0
French Grandpa <pepijn.pinckers@hotmail.nl>
2019-02-27 11:25:02 +0100

    Added Grid

--- Server/src/main/java/zeeslag/server/Grid.java
+++ Server/src/main/java/zeeslag/server/Grid.java
@@ -1,4 +1,48 @@
 package zeeslag.server;
 
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
 public class Grid {
+    public final int width;
+    public final int height;
+    public final Tile[][] tiles;
+
+    public Grid(final int width, final int height) {
+        this.width = width;
+        this.height = height;
+
+        tiles = new Tile[height][];
+        for (int y = 0; y < height; ++y) {
+            tiles[y] = new Tile[width];
+        }
+    }
+
+    @Nullable
+    public Tile at(@NotNull final Position position) {
+        final Position normalized = normalize(position);
+        return tiles[normalized.y][normalized.x];
+    }
+
+    public int calculateDistance(@NotNull final Position source, @NotNull final Position target) {
+        final Position normalizedSource = normalize(source);
+        final Position normalizedTarget = normalize(target);
+
+        final int dx = Math.abs(normalizedSource.x - normalizedTarget.x);
+        final int dy = Math.abs(normalizedSource.y - normalizedTarget.y);
+
+        final int toroidal_dx = Math.min(dx, width - dx);
+        final int toroidal_dy = Math.min(dy, height - dy);
+
+        return toroidal_dx + toroidal_dy;
+    }
+
+    @NotNull
+    public Position normalize(@NotNull final Position position) {
+        final int x = ((position.x % width) + width) % width;
+        final int y = ((position.y % height) + height) % height;
+        return new Position(x, y);
+    }
+
+
 }
